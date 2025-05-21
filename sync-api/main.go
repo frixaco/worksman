22de9533ec6897d3c8b6119d860fa4cc4f724707
frixaco <rustam21ashurmatov@gmail.com>
2025-05-22 00:26:57 +0500

    Fix CORS in sync api

--- sync-api/main.go
+++ sync-api/main.go
@@ -5,12 +5,25 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	
+	// Configure CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+	
 	const path = "/data/data.json"
 
 	r.POST("/sync", func(ctx *gin.Context) {
